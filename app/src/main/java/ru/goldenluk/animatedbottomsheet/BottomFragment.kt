92ccfcb43ca2c3498c1492ceb79b85cead60788e
Matvei Khadzhiev <khad-mat123@yandex.ru>
2021-07-13 12:12:15 +0300

    Initiate

---
+++ app/src/main/java/ru/goldenluk/animatedbottomsheet/BottomFragment.kt
@@ -0,0 +1,110 @@
+package ru.goldenluk.animatedbottomsheet
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import ru.goldenluk.animatedbottomsheet.databinding.BottomSheetLayoutBinding
+
+private const val COLLAPSED_HEIGHT = 228
+
+class BottomFragment : BottomSheetDialogFragment() {
+
+    // Можно обойтись без биндинга и использовать findViewById
+    lateinit var binding: BottomSheetLayoutBinding
+
+    // Переопределим тему, чтобы использовать нашу с закруглёнными углами
+    override fun getTheme() = R.style.AppBottomSheetDialogTheme
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = BottomSheetLayoutBinding.bind(inflater.inflate(R.layout.bottom_sheet_layout, container))
+        return binding.root
+    }
+
+    // Я выбрал этот метод ЖЦ, и считаю, что это удачное место
+    // Вы можете попробовать производить эти действия не в этом методе ЖЦ, а например в onCreateDialog()
+    override fun onStart() {
+        super.onStart()
+
+        // Плотность понадобится нам в дальнейшем
+        val density = requireContext().resources.displayMetrics.density
+
+        dialog?.let {
+            // Находим сам bottomSheet и достаём из него Behaviour
+            val bottomSheet = it.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+            val behavior = BottomSheetBehavior.from(bottomSheet)
+
+            // Выставляем высоту для состояния collapsed и выставляем состояние collapsed
+            behavior.peekHeight = (COLLAPSED_HEIGHT * density).toInt()
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+            // Достаём корневые лэйауты
+            val coordinator = (it as BottomSheetDialog).findViewById<CoordinatorLayout>(com.google.android.material.R.id.coordinator)
+            val containerLayout = it.findViewById<FrameLayout>(com.google.android.material.R.id.container)
+
+            // Надуваем наш лэйаут с кнопкой
+            val buttons = it.layoutInflater.inflate(R.layout.button, null)
+
+            // Выставояем параметры для нашей кнопки
+            buttons.layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                height = (60 * density).toInt()
+                gravity = Gravity.BOTTOM
+            }
+            // Добавляем кнопку в контейнер
+            containerLayout?.addView(buttons)
+
+            // Перерисовываем лэйаут
+            buttons.post {
+                (coordinator?.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                    buttons.measure(
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+                    )
+                    // Устраняем разрыв между кнопкой и скролящейся частью
+                    this.bottomMargin = (buttons.measuredHeight - 8 * density).toInt()
+                    containerLayout?.requestLayout()
+                }
+            }
+
+            behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    // Нам не нужны действия по этому колбеку
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    with(binding) {
+                        // Нас интересует только положительный оффсет, тк при отрицательном нас устроит стандартное поведение - скрытие фрагмента
+                        if (slideOffset > 0) {
+                            // Делаем "свёрнутый" layout более прозрачным
+                            layoutCollapsed.alpha = 1 - 2 * slideOffset
+                            // И в то же время делаем "расширенный layout" менее прозрачным
+                            layoutExpanded.alpha = slideOffset * slideOffset
+
+                            // Когда оффсет превышает половину, мы скрываем collapsed layout и делаем видимым expanded
+                            if (slideOffset > 0.5) {
+                                layoutCollapsed.visibility = View.GONE
+                                layoutExpanded.visibility = View.VISIBLE
+                            }
+
+                            // Если же оффсет меньше половины, а expanded layout всё ещё виден, то нужно скрывать его и показывать collapsed
+                            if (slideOffset < 0.5 && binding.layoutExpanded.visibility == View.VISIBLE) {
+                                layoutCollapsed.visibility = View.VISIBLE
+                                layoutExpanded.visibility = View.INVISIBLE
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }
+}
---
+++ app/src/main/java/ru/goldenluk/animatedbottomsheet/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.goldenluk.animatedbottomsheet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        BottomFragment().show(supportFragmentManager, "tag")
+    }
+}
